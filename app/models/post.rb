3fd9949ea77e0fe993308789c21609a9acb3ba32
Bill <billfeng2002@gmail.com>
2021-07-13 11:49:29 -0700

    commit tree 4746582994433316e8628dfa0b895b574ed4a383

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: :true
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"] }
+    validate :clickbait
+
+    def clickbait
+        baits=["Won't Believe", "Secret", /Top \d/, "Guess"]
+        baits.each{|bait|
+            break if !title
+            if(bait.class==Regexp)
+                return if title.match(bait)
+            end
+            if(bait.class==String)
+                return if title.include?(bait)
+            end
+        }
+        errors[:clickbait]="Not Clickbaity enough"
+    end
 end
